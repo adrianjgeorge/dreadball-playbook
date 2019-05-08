c25b84928f87c9308e21d7d131555eceb6a6c9f1
Adrian George <adrian.george@rackspace.com>
2019-05-07 21:57:52 -0500

    Added rest project from spring initializer

---
+++ dreadball-playbook-rest/src/test/kotlin/com/pantsware/dreadballplaybook/rest/DreadballPlaybookRestApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.pantsware.dreadballplaybook.rest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DreadballPlaybookRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
